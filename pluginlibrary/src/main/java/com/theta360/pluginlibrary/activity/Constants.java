22feb113bdf0c8f4b867427d472c9dff88eeb1c0
you-tiro <yuuichiroh.shimizu@jrits.ricoh.co.jp>
2018-12-17 09:47:49 +0900

    add USER_OPTION / IS_AP_CONNECTED

---
+++ pluginlibrary/src/main/java/com/theta360/pluginlibrary/activity/Constants.java
@@ -0,0 +1,36 @@
+package com.theta360.pluginlibrary.activity;
+
+final class Constants {
+    static final String ACTION_MAIN_CAMERA_CLOSE = "com.theta360.plugin.ACTION_MAIN_CAMERA_CLOSE";
+    static final String ACTION_MAIN_CAMERA_OPEN = "com.theta360.plugin.ACTION_MAIN_CAMERA_OPEN";
+    static final String ACTION_FINISH_PLUGIN = "com.theta360.plugin.ACTION_FINISH_PLUGIN";
+    static final String ACTION_ERROR_OCCURED = "com.theta360.plugin.ACTION_ERROR_OCCURED";
+    static final String PACKAGE_NAME = "packageName";
+    static final String EXIT_STATUS = "exitStatus";
+    static final String MESSAGE = "message";
+
+    static final String ACTION_LED_SHOW = "com.theta360.plugin.ACTION_LED_SHOW";
+    static final String ACTION_LED_BLINK = "com.theta360.plugin.ACTION_LED_BLINK";
+    static final String ACTION_LED_HIDE = "com.theta360.plugin.ACTION_LED_HIDE";
+    static final String TARGET = "target";
+    static final String COLOR = "color";
+    static final String PERIOD = "period";
+
+    static final String ACTION_AUDIO_SHUTTER = "com.theta360.plugin.ACTION_AUDIO_SHUTTER";
+    static final String ACTION_AUDIO_SH_OPEN = "com.theta360.plugin.ACTION_AUDIO_SH_OPEN";
+    static final String ACTION_AUDIO_SH_CLOSE = "com.theta360.plugin.ACTION_AUDIO_SH_CLOSE";
+    static final String ACTION_AUDIO_MOVSTART = "com.theta360.plugin.ACTION_AUDIO_MOVSTART";
+    static final String ACTION_AUDIO_MOVSTOP = "com.theta360.plugin.ACTION_AUDIO_MOVSTOP";
+    static final String ACTION_AUDIO_SELF = "com.theta360.plugin.ACTION_AUDIO_SELF";
+    static final String ACTION_AUDIO_WARNING = "com.theta360.plugin.ACTION_AUDIO_WARNING";
+
+    static final String ACTION_WLAN_OFF = "com.theta360.plugin.ACTION_WLAN_OFF";
+    static final String ACTION_WLAN_AP = "com.theta360.plugin.ACTION_WLAN_AP";
+    static final String ACTION_WLAN_CL = "com.theta360.plugin.ACTION_WLAN_CL";
+
+    static final String ACTION_DATABASE_UPDATE = "com.theta360.plugin.ACTION_DATABASE_UPDATE";
+    static final String TARGETS = "targets";
+
+    static final String USER_OPTION = "USER_OPTION";
+    static final String IS_AP_CONNECTED = "IS_AP_CONNECTED";
+}
--- pluginlibrary/src/main/java/com/theta360/pluginlibrary/activity/PluginActivity.java
+++ pluginlibrary/src/main/java/com/theta360/pluginlibrary/activity/PluginActivity.java
@@ -35,39 +35,12 @@ import com.theta360.pluginlibrary.values.LedTarget;
  * PluginActivity
  */
 public abstract class PluginActivity extends AppCompatActivity {
-    private static final String ACTION_MAIN_CAMERA_CLOSE = "com.theta360.plugin.ACTION_MAIN_CAMERA_CLOSE";
-    private static final String ACTION_MAIN_CAMERA_OPEN = "com.theta360.plugin.ACTION_MAIN_CAMERA_OPEN";
-    private static final String ACTION_FINISH_PLUGIN = "com.theta360.plugin.ACTION_FINISH_PLUGIN";
-    private static final String ACTION_ERROR_OCCURED = "com.theta360.plugin.ACTION_ERROR_OCCURED";
-    private static final String PACKAGE_NAME = "packageName";
-    private static final String EXIT_STATUS = "exitStatus";
-    private static final String MESSAGE = "message";
-
-    private static final String ACTION_LED_SHOW = "com.theta360.plugin.ACTION_LED_SHOW";
-    private static final String ACTION_LED_BLINK = "com.theta360.plugin.ACTION_LED_BLINK";
-    private static final String ACTION_LED_HIDE = "com.theta360.plugin.ACTION_LED_HIDE";
-    private static final String TARGET = "target";
-    private static final String COLOR = "color";
-    private static final String PERIOD = "period";
-
-    private static final String ACTION_AUDIO_SHUTTER = "com.theta360.plugin.ACTION_AUDIO_SHUTTER";
-    private static final String ACTION_AUDIO_SH_OPEN = "com.theta360.plugin.ACTION_AUDIO_SH_OPEN";
-    private static final String ACTION_AUDIO_SH_CLOSE = "com.theta360.plugin.ACTION_AUDIO_SH_CLOSE";
-    private static final String ACTION_AUDIO_MOVSTART = "com.theta360.plugin.ACTION_AUDIO_MOVSTART";
-    private static final String ACTION_AUDIO_MOVSTOP = "com.theta360.plugin.ACTION_AUDIO_MOVSTOP";
-    private static final String ACTION_AUDIO_SELF = "com.theta360.plugin.ACTION_AUDIO_SELF";
-    private static final String ACTION_AUDIO_WARNING = "com.theta360.plugin.ACTION_AUDIO_WARNING";
-
-    private static final String ACTION_WLAN_OFF = "com.theta360.plugin.ACTION_WLAN_OFF";
-    private static final String ACTION_WLAN_AP = "com.theta360.plugin.ACTION_WLAN_AP";
-    private static final String ACTION_WLAN_CL = "com.theta360.plugin.ACTION_WLAN_CL";
-
-    private static final String ACTION_DATABASE_UPDATE = "com.theta360.plugin.ACTION_DATABASE_UPDATE";
-    private static final String TARGETS = "targets";
-
     private boolean isCamera = false;
     private boolean isAutoClose = true;
     private boolean isClosed = false;
+    private String mUserOption;
+    private boolean isApConnected = false;
+
     private KeyCallback mKeyCallback;
     private KeyReceiver mKeyReceiver;
     private KeyReceiver.Callback onKeyReceiver = new KeyReceiver.Callback() {
@@ -119,6 +92,12 @@ public abstract class PluginActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        Intent intent = getIntent();
+        if (intent != null) {
+            mUserOption = intent.getStringExtra(Constants.USER_OPTION);
+            isApConnected = intent.getBooleanExtra(Constants.IS_AP_CONNECTED, false);
+        }
+
         mKeyReceiver = new KeyReceiver(onKeyReceiver);
         IntentFilter keyFilter = new IntentFilter();
         keyFilter.addAction(KeyReceiver.ACTION_KEY_DOWN);
@@ -160,63 +139,72 @@ public abstract class PluginActivity extends AppCompatActivity {
         notificationSuccess();
     }
 
+    public String getUserOption() {
+        return mUserOption;
+    }
+
+    public boolean isApConnected() {
+        return isApConnected;
+    }
+
+
     public void notificationCameraOpen() {
         isCamera = false;
-        sendBroadcast(new Intent(ACTION_MAIN_CAMERA_OPEN));
+        sendBroadcast(new Intent(Constants.ACTION_MAIN_CAMERA_OPEN));
     }
 
     public void notificationCameraClose() {
         isCamera = true;
-        sendBroadcast(new Intent(ACTION_MAIN_CAMERA_CLOSE));
+        sendBroadcast(new Intent(Constants.ACTION_MAIN_CAMERA_CLOSE));
     }
 
     /**
      * Sound of normal capture
      */
     public void notificationAudioShutter() {
-        sendBroadcast(new Intent(ACTION_AUDIO_SHUTTER));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_SHUTTER));
     }
 
     /**
      * Sound of starting long exposure capture
      */
     public void notificationAudioOpen() {
-        sendBroadcast(new Intent(ACTION_AUDIO_SH_OPEN));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_SH_OPEN));
     }
 
     /**
      * Sound of ending long exposure capture
      */
     public void notificationAudioClose() {
-        sendBroadcast(new Intent(ACTION_AUDIO_SH_CLOSE));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_SH_CLOSE));
     }
 
     /**
      * Sound of starting movie recording
      */
     public void notificationAudioMovStart() {
-        sendBroadcast(new Intent(ACTION_AUDIO_MOVSTART));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_MOVSTART));
     }
 
     /**
      * Sound of stopping movie recording
      */
     public void notificationAudioMovStop() {
-        sendBroadcast(new Intent(ACTION_AUDIO_MOVSTOP));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_MOVSTOP));
     }
 
     /**
      * Sound of working self-timer
      */
     public void notificationAudioSelf() {
-        sendBroadcast(new Intent(ACTION_AUDIO_SELF));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_SELF));
     }
 
     /**
      * Sound of warning
      */
     public void notificationAudioWarning() {
-        sendBroadcast(new Intent(ACTION_AUDIO_WARNING));
+        sendBroadcast(new Intent(Constants.ACTION_AUDIO_WARNING));
     }
 
     /**
@@ -225,9 +213,9 @@ public abstract class PluginActivity extends AppCompatActivity {
      * @param ledColor target LED
      */
     public void notificationLed3Show(@NonNull LedColor ledColor) {
-        Intent intent = new Intent(ACTION_LED_SHOW);
-        intent.putExtra(TARGET, LedTarget.LED3.toString());
-        intent.putExtra(COLOR, ledColor.toString());
+        Intent intent = new Intent(Constants.ACTION_LED_SHOW);
+        intent.putExtra(Constants.TARGET, LedTarget.LED3.toString());
+        intent.putExtra(Constants.COLOR, ledColor.toString());
         sendBroadcast(intent);
     }
 
@@ -240,8 +228,8 @@ public abstract class PluginActivity extends AppCompatActivity {
         if (ledTarget == LedTarget.LED3) {
             notificationLed3Show(LedColor.BLUE);
         } else {
-            Intent intent = new Intent(ACTION_LED_SHOW);
-            intent.putExtra(TARGET, ledTarget.toString());
+            Intent intent = new Intent(Constants.ACTION_LED_SHOW);
+            intent.putExtra(Constants.TARGET, ledTarget.toString());
             sendBroadcast(intent);
         }
     }
@@ -264,10 +252,10 @@ public abstract class PluginActivity extends AppCompatActivity {
             period = 2000;
         }
 
-        Intent intent = new Intent(ACTION_LED_BLINK);
-        intent.putExtra(TARGET, ledTarget.toString());
-        intent.putExtra(COLOR, ledColor.toString());
-        intent.putExtra(PERIOD, period);
+        Intent intent = new Intent(Constants.ACTION_LED_BLINK);
+        intent.putExtra(Constants.TARGET, ledTarget.toString());
+        intent.putExtra(Constants.COLOR, ledColor.toString());
+        intent.putExtra(Constants.PERIOD, period);
         sendBroadcast(intent);
     }
 
@@ -277,26 +265,26 @@ public abstract class PluginActivity extends AppCompatActivity {
      * @param ledTarget target LED
      */
     public void notificationLedHide(@NonNull LedTarget ledTarget) {
-        Intent intent = new Intent(ACTION_LED_HIDE);
-        intent.putExtra(TARGET, ledTarget.toString());
+        Intent intent = new Intent(Constants.ACTION_LED_HIDE);
+        intent.putExtra(Constants.TARGET, ledTarget.toString());
         sendBroadcast(intent);
     }
 
     public void notificationWlanOff() {
-        sendBroadcast(new Intent(ACTION_WLAN_OFF));
+        sendBroadcast(new Intent(Constants.ACTION_WLAN_OFF));
     }
 
     public void notificationWlanAp() {
-        sendBroadcast(new Intent(ACTION_WLAN_AP));
+        sendBroadcast(new Intent(Constants.ACTION_WLAN_AP));
     }
 
     public void notificationWlanCl() {
-        sendBroadcast(new Intent(ACTION_WLAN_CL));
+        sendBroadcast(new Intent(Constants.ACTION_WLAN_CL));
     }
 
     public void notificationDatabaseUpdate(@NonNull String[] targets) {
-        Intent intent = new Intent(ACTION_DATABASE_UPDATE);
-        intent.putExtra(TARGETS, targets);
+        Intent intent = new Intent(Constants.ACTION_DATABASE_UPDATE);
+        intent.putExtra(Constants.TARGETS, targets);
         sendBroadcast(intent);
     }
 
@@ -304,9 +292,9 @@ public abstract class PluginActivity extends AppCompatActivity {
      * Notifying Completion of Plug-in when the plug-in ends normally
      */
     public void notificationSuccess() {
-        Intent intent = new Intent(ACTION_FINISH_PLUGIN);
-        intent.putExtra(PACKAGE_NAME, getPackageName());
-        intent.putExtra(EXIT_STATUS, ExitStatus.SUCCESS.toString());
+        Intent intent = new Intent(Constants.ACTION_FINISH_PLUGIN);
+        intent.putExtra(Constants.PACKAGE_NAME, getPackageName());
+        intent.putExtra(Constants.EXIT_STATUS, ExitStatus.SUCCESS.toString());
         sendBroadcast(intent);
         finishAndRemoveTask();
     }
@@ -317,10 +305,10 @@ public abstract class PluginActivity extends AppCompatActivity {
      * @param message error message
      */
     public void notificationError(String message) {
-        Intent intent = new Intent(ACTION_FINISH_PLUGIN);
-        intent.putExtra(PACKAGE_NAME, getPackageName());
-        intent.putExtra(EXIT_STATUS, ExitStatus.FAILURE.toString());
-        intent.putExtra(MESSAGE, message);
+        Intent intent = new Intent(Constants.ACTION_FINISH_PLUGIN);
+        intent.putExtra(Constants.PACKAGE_NAME, getPackageName());
+        intent.putExtra(Constants.EXIT_STATUS, ExitStatus.FAILURE.toString());
+        intent.putExtra(Constants.MESSAGE, message);
         sendBroadcast(intent);
         finishAndRemoveTask();
     }
@@ -329,6 +317,6 @@ public abstract class PluginActivity extends AppCompatActivity {
      * Notifying Occurrences of Errors
      */
     public void notificationErrorOccured() {
-        sendBroadcast(new Intent(ACTION_ERROR_OCCURED));
+        sendBroadcast(new Intent(Constants.ACTION_ERROR_OCCURED));
     }
 }
