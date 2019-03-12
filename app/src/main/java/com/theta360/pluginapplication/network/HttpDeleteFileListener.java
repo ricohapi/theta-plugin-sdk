242a35e63f741b1f8276e1c22869133e67030cfd
Tom Raybould <traybould@earthcam.com>
2019-03-12 12:38:04 -0400

    Deleteing files now complies with api v2.1

---
+++ app/src/main/java/com/theta360/pluginapplication/network/HttpDeleteFileListener.java
@@ -0,0 +1,33 @@
+package com.theta360.pluginapplication.network;
+
+import java.util.ArrayList;
+
+/**
+ * Created by traybould on 3/12/2019.
+ */
+
+public interface HttpDeleteFileListener {
+
+	/**
+	 * Notifies you of the device status check results
+	 * @param newStatus true:Update available, false;No update available
+	 */
+	void onCheckStatus(boolean newStatus);
+
+	/**
+	 * Notifies you when the files are deleted
+	 * @param deletedImages Urls of images that were deleted
+	 * */
+	void onObjectChanged(ArrayList<String> deletedImages);
+
+	/**
+	 * Notify on completion of event
+	 */
+	void onCompleted();
+
+	/**
+	 * Notify in the event of an error
+	 */
+	void onError(String errorMessage);
+
+}
